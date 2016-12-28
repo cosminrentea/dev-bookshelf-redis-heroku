608b02b3816a4d6bb972ab048d52d2199de6175d
Cosmin Rentea <c.rentea@tarent.de>
2016-12-28 17:56:54 +0200

    Sonar fixes  UrlUtil; using logger

--- src/main/java/com/chrisbaileydeveloper/bookshelf/web/util/UrlUtil.java
+++ src/main/java/com/chrisbaileydeveloper/bookshelf/web/util/UrlUtil.java
@@ -4,27 +4,33 @@ import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
 public class UrlUtil {
 
+    private static final Logger logger = LoggerFactory.getLogger(UrlUtil.class);
+
     private UrlUtil() {
         // intentionally empty
     }
 
-    public static String encodeUrlPathSegment(String pathSegment,
-                                              HttpServletRequest httpServletRequest) {
+    public static String encodeUrlPathSegment(final String pathSegment, final HttpServletRequest httpServletRequest) {
 
-        String enc = httpServletRequest.getCharacterEncoding();
+        String characterEncoding = httpServletRequest.getCharacterEncoding();
 
-        if (enc == null)
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {
+        if (characterEncoding == null) {
+            characterEncoding = WebUtils.DEFAULT_CHARACTER_ENCODING;
         }
-        return pathSegment;
+
+        String ps = pathSegment;
+        try {
+            ps = UriUtils.encodePathSegment(pathSegment, characterEncoding);
+        } catch (final UnsupportedEncodingException uee) {
+            logger.warn("could not encode path segment", uee);
+        }
+        return ps;
     }
 }
